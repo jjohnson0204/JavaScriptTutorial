3a4e6297947590b2e391c40c1c26e0e98a224b01
jjohnson0204 <78010828+jjohnson0204@users.noreply.github.com>
2024-01-09 23:05:11 -0500

    Math Object

---
+++ Math Object/index.js
@@ -0,0 +1,57 @@
+// Math = built-in object that provides a
+//          collection of properties and methods
+
+Math.PI
+
+console.log(Math.PI);
+console.log(Math.E);
+
+let x = 3;
+let x1 = 3.99;
+
+let y = 2;
+
+let z = 1;
+let z1;
+let z2;
+let z3;
+let z4;
+let z5;
+let z6;
+let z7;
+let z8
+let z9;
+let z10;
+let z11;
+
+z = Math.round(x);
+z1 = Math.floor(x);
+z2 = Math.ceil(x);
+z3 = Math.trunc(x);
+z4 = Math.pow(x, y);
+z5 = Math.sqrt(x);
+z6 = Math.log(x);
+z7 = Math.sin(x);
+z8 = Math.cos(x);
+z9 = Math.tan(x);
+z10 = Math.abs(x);
+z11 = Math.sign(x);
+
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
+
+console.log(z);
+console.log(z1);
+console.log(z2);
+console.log(z3);
+console.log(z4);
+console.log(z5);
+console.log(z6);
+console.log(z7);
+console.log(z8)
+console.log(z9);
+console.log(z10);
+console.log(z11);
+
+console.log(max);
+console.log(min);
